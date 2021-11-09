37d68f6844b5152485ce0318fc7d5114cb83619b
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-09 10:32:53 +0530

    update

--- src/main/java/com/cloudapps/developer/GatewayConfig.java
+++ src/main/java/com/cloudapps/developer/GatewayConfig.java
@@ -7,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-	
-	
+
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		
